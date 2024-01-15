67ea6027f14a299846158f7d146db70146829a19
julianbaladjay <julian_baladjay@dlsu.edu.ph>
2024-01-15 20:16:04 +0800

    Add project files.

---
+++ (Baladjay) Assignment 01.cpp
@@ -0,0 +1,47 @@
+#include <GLFW/glfw3.h>
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Julian H. Baladjay", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_POLYGON);
+
+        glVertex2f(0.0f, 0.5f);
+        glVertex2f(0.4755f, 0.1545f);
+        glVertex2f(0.2939f, -0.4045f);
+        glVertex2f(-0.2939f, -0.4045f);
+        glVertex2f(-0.4755f, 0.1545f);
+
+        glEnd();
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
